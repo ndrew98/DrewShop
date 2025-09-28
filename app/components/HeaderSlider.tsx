92d54c07bdf192ad02f2e550b7a9492d1148502a
Andrew <laryeaandrew0001@gmail.com>
2025-09-28 14:32:37 +0000

    HeaderSlider type definition

--- app/components/HeaderSlider.tsx
+++ app/components/HeaderSlider.tsx
@@ -39,7 +39,7 @@ const HeaderSlider = () => {
     return () => clearInterval(interval);
   }, [sliderData.length]);
 
-  const handleSlideChange = (index) => {
+  const handleSlideChange = (index: number) => {
     setCurrentSlide(index);
   };
 
